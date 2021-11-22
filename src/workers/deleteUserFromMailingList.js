d20b47c044296a131ba7479da8b136d0d0e43949
Chris Bongers <chrisbongers@gmail.com>
2021-11-22 19:50:03 +0400

    fix: parsed the worker data wrong (#385)

--- src/workers/deleteUserFromMailingList.js
+++ src/workers/deleteUserFromMailingList.js
@@ -6,19 +6,19 @@ const worker = {
   subscription: 'user-deleted-mailing',
   handler: async (message, log) => {
     const data = messageToJson(message);
-    if (!data.user.email) {
+    if (!data.email) {
       log.warn(
-        { messageId: message.id, userId: data.user.id },
+        { messageId: message.id, userId: data.id },
         'no email in user-deleted message',
       );
       return;
     }
     try {
-      const contactId = await getContactIdByEmail(data.user.email);
+      const contactId = await getContactIdByEmail(data.email);
       await removeUserContact(contactId);
     } catch (err) {
       log.error(
-        { messageId: message.id, err, userId: data.user.id },
+        { messageId: message.id, err, userId: data.id },
         'failed to delete user from mailing list',
       );
       throw err;
