479a25abb75117459c0228aef9f65edd1ae5d02d
Lee Hansel Solevilla <sshanzel@yahoo.com>
2021-12-21 17:37:22 +0800

    fix: gateway to api integration using secret (#396)
    Using api-secret to integrate with the `daily-api` service for fetching user alerts and settings.

--- src/integration.js
+++ src/integration.js
@@ -1,12 +1,13 @@
 import rp from 'request-promise-native';
 import config from './config';
 
-export const getFromDailyApi = async (ctx, query) => {
+export const getFromDailyApi = async (ctx, query, headers = {}) => {
   const res = await rp({
     method: 'POST',
     url: `${config.apiUrl}/graphql`,
     body: JSON.stringify({ query }),
     headers: {
+      ...headers,
       cookie: ctx.request.header.cookie,
       'content-type': 'application/json',
     },
@@ -16,6 +17,7 @@ export const getFromDailyApi = async (ctx, query) => {
 };
 
 export const getSettingsFromAPI = (ctx) => {
+  const headers = { authorization: `Service ${config.apiSecret}` };
   const query = `{
     userSettings {
       openNewTab
@@ -28,10 +30,11 @@ export const getSettingsFromAPI = (ctx) => {
     }
   }`;
 
-  return getFromDailyApi(ctx, query);
+  return getFromDailyApi(ctx, query, headers);
 };
 
 export const getAlertsFromAPI = (ctx) => {
+  const headers = { authorization: `Service ${config.apiSecret}` };
   const query = `{
     userAlerts {
       filter
@@ -39,5 +42,5 @@ export const getAlertsFromAPI = (ctx) => {
     }
   }`;
 
-  return getFromDailyApi(ctx, query);
+  return getFromDailyApi(ctx, query, headers);
 };
